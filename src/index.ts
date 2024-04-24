e37ca7556c50810113de44a25bbf7ff511aa7594
Prem <premkumar@divami.com>
2024-04-24 23:36:34 +0530

    basic setup for express and apolo graphql

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { initialiseServer } from "./App";
+
+async function init() {
+  const app = await initialiseServer();
+  app.listen(8000, () => console.log("App is running at 8000"));
+}
+
+init()
+  .then(() => console.log("no error"))
+  .catch((e) => console.log("opl"));
