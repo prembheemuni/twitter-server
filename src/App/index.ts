dad41c59e330259283c948bfcfbc5961b927f531
Prem <premkumar@divami.com>
2024-05-05 19:33:23 +0530

    Added cors

--- src/App/index.ts
+++ src/App/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import bodyParser from "body-parser";
-
+import cors from "cors";
 import { User } from "./user";
 
 export async function initialiseServer() {
   const app = express();
+
+  app.use(cors());
 
   app.use(bodyParser.json());
 
